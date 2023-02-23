013a2ec6b4e03420229435b3144473c91183f848
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-02-23 04:30:14 +0000

    add another isogram solution

--- fsharp/isogram/Isogram.fs
+++ fsharp/isogram/Isogram.fs
@@ -1,5 +1,7 @@
 module Isogram
 
+open System
+
 let isIsogram (str: string) =
     let oneSpaceOrHyphen =
         System.Text.RegularExpressions.Regex.Replace(str.ToLower(), "[- ]", "")
@@ -11,6 +13,21 @@ isIsogram "background"
 isIsogram "downstream"
 isIsogram "six-year-old"
 isIsogram "isograms"
-isIsogram "isograms"
 isIsogram "Alphabet"
 isIsogram "alphAbet"
+
+let isIsogram2 (str: string) =
+    str
+    |> Seq.filter Char.IsLetter
+    |> Seq.countBy Char.ToLower
+    |> Seq.forall (fun count -> snd count = 1)
+
+let str1 = "downstreamO"
+let str2 = "six-year-old"
+let str3 = "isograms"
+let str4 = "Alphabet"
+
+isIsogram2 str1
+isIsogram2 str2
+isIsogram2 str3
+isIsogram2 str4
