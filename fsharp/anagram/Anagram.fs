f73f6f7d8842888b5e71e843f1cc736573c5a7f1
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-03-06 11:32:32 +0000

    update solution to avoid using map

--- fsharp/anagram/Anagram.fs
+++ fsharp/anagram/Anagram.fs
@@ -3,11 +3,11 @@ module Anagram
 let findAnagrams (sources: string list) (target: string) =
     let lower = target.ToLower()
 
-    let sameLength =
+    let sameLengthAsLowercase =
         sources
         |> List.filter (fun x -> x.Length = lower.Length && x.ToLower() <> target.ToLower())
 
-    if sameLength.Length = 0 then
+    if sameLengthAsLowercase.Length = 0 then
         []
     else
         let count str =
@@ -16,17 +16,31 @@ let findAnagrams (sources: string list) (target: string) =
             |> Seq.countBy (fun x -> System.Char.ToLower(x))
             |> Seq.sort
 
-        let targetCount = Map(count target)
+        let targetCount = count target
 
-        let isAnagram (toCheck: string) = Map(count toCheck) = targetCount
+        let isAnagram (toCheck: string) =
+            (Seq.compareWith Operators.compare (count toCheck) targetCount) = 0
 
-        sameLength |> List.filter isAnagram
-
+        sameLengthAsLowercase |> List.filter isAnagram
 
 (*
+    Testing grounds!
 
+    // Testing the count function.
+    let count str =
+        str
+        |> Seq.filter (fun x -> x.ToString().Trim() <> "")
+        |> Seq.countBy (fun x -> System.Char.ToLower(x))
+        |> Seq.sort
+
+    Seq.compareWith Operators.compare (count "hey") (count "eyh")
+
+    // No anagrams.
     let candidates = [ "hello"; "world"; "zombies"; "pants" ]
     findAnagrams candidates "diaper"
 
+    // Anagrams.
+    let candidates = [ "hey"; "eyh"; "hahaha" ]
+    findAnagrams candidates "ehy"
 
 *)
