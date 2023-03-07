1a51fc07a2191805ae69f3597f63e6c7b3435703
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-03-07 02:30:01 +0000

    [vlang] add solution to leap year problem

--- vlang/leap/leap.v
+++ vlang/leap/leap.v
@@ -1,6 +1,26 @@
 module main
 
+fn is_evently_divisible(year int) bool {
+	return year % 4 == 0
+}
+
 // is_leap_year returns true if the given year is a leap year in the Gregorian calendar
 fn is_leap_year(year int) bool {
-	
+	// Not evenly divisible by 4? Not leap.
+	if year % 4 != 0 {
+		return false
+	}
+
+	// Evenly divisible by 4 but not by 100? Leap year.
+	if year % 100 != 0 {
+		return true
+	}
+
+	// Evenly divisible by 4 and by 100, but not by 400? Not a leap year.
+	if year % 400 != 0 {
+		return false
+	}
+
+	// Evenly divisible by 4, 100 and 400? Leap year.
+	return true
 }
