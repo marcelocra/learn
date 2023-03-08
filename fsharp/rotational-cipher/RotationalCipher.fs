1fc4633f1d716317376c17351799ff932a0ed12c
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-03-08 11:48:41 +0000

    [fsharp] solve rotational-cypher

--- fsharp/rotational-cipher/RotationalCipher.fs
+++ fsharp/rotational-cipher/RotationalCipher.fs
@@ -2,14 +2,50 @@ module RotationalCipher
 
 open System
 
-let rotate (shiftKey: int) (text: string) =
-    text.ToCharArray()
-    |> Array.map(fun x -> Char.)
-
 
 (*
-    Testing grounds!
+    Algorithm ideas:
 
-    printfn "%A" (System.Char.GetNumericValue('a'))
+    1. Define bounds for each character type (lowercase, uppercase, numbers, etc.)
+    2. 
+*)
+
+let shift (key: char) (shift: int) =
+    let lower = Char.IsLower key
+    let keyInt = int key
+    let lowerBound = if lower then int 'a' else int 'A'
+    let upperBound = if lower then int 'z' else int 'Z'
+    let shifted = keyInt + shift
+
+    if shifted > upperBound then
+        char (lowerBound + (shifted - upperBound) - 1)
+    else
+        char shifted
+
+let rotate (shiftKey: int) (text: string) =
+    text |> String.map (fun c -> if Char.IsLetter c then shift c shiftKey else c)
+
+(*
+
+// Testing grounds! Comment out before submitting.
+
+open System
+
+"test".ToCharArray()
+char ((int 'a') + 10)
+
+let rot = 10
+(int 'A')
+(int 'Z')
+100 % 90
+(char ((int 'A') - 1 + ((int 'Z') + 10) % 90))
+shift 'a' 10 = 'k'
+shift 'z' 10 = 'j'
+shift 'Z' 10 = 'J'
+rotate 5 "omg"
+rotate 0 "c"
+rotate 26 "Cool"
+rotate 13 "The quick brown fox jumps over the lazy dog."
+rotate 13 "Gur dhvpx oebja sbk whzcf bire gur ynml qbt."
 
 *)
