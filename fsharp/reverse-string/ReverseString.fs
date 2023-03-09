3926ee5dc9ad94fe90e11f4ecf9402a233e230da
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-03-09 00:52:11 +0000

    [fsharp] add new solution to reverse string problem

--- fsharp/reverse-string/ReverseString.fs
+++ fsharp/reverse-string/ReverseString.fs
@@ -1,4 +1,12 @@
 module ReverseString
 
-let reverse (input: string) =
+let reverseCheating (input: string) =
     input.ToCharArray() |> Array.rev |> System.String
+
+let reverse (input: string) =
+    let rec reverse' s out =
+        match s with
+        | x :: r -> reverse' r (x :: out)
+        | [] -> out
+
+    reverse' (input |> List.ofSeq) [] |> System.String.Concat
