f194d45a035a27d1c7a9c7dfa4f0757d6175af25
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-03-21 03:11:34 +0000

    [fsharp] solve the allergies exercise

--- tracks/fsharp/allergies/Allergies.fs
+++ tracks/fsharp/allergies/Allergies.fs
@@ -3,7 +3,6 @@ module Allergies
 open System
 
 
-// TODO: define the Allergen type
 type Allergen =
     | Eggs
     | Peanuts
@@ -14,35 +13,39 @@ type Allergen =
     | Pollen
     | Cats
 
+// let i = match allergen with
+//     | Eggs -> 1
+//     | Peanuts -> 2
+//     | Shellfish -> 4
+//     | Strawberries -> 8
+//     | Tomatoes -> 16
+//     | Chocolate -> 32
+//     | Pollen -> 64
+//     | Cats -> 128
 
-let rec allergicTo codedAllergies allergen =
-    // match codedAllergies with
-    // | i when i < 0 -> false
-    // | 0 -> true
-    let i = match allergen with
-        | Eggs -> 1
-        | Peanuts -> 2
-        | Shellfish -> 4
-        | Strawberries -> 8
-        | Tomatoes -> 16
-        | Chocolate -> 32
-        | Pollen -> 64
-        | Cats -> 128
-
-    | i when i >= 256 -> allergicTo (i - 256) allergen
-    | i when i >= 128 && i < 256 -> allergicTo (codedAllergies - 128) allergen
-    | i when i >= 64 && i < 128 -> allergicTo (codedAllergies - 64) allergen
-    | i when i >= 32 && i < 64 -> allergicTo (codedAllergies - 32) allergen
-    | i when i >= 16 && i < 32 -> allergicTo (codedAllergies - 16) allergen
-    | i when i >= 8 && i < 16 -> allergicTo (codedAllergies - 8) allergen
-    | i when i >= 4 && i < 8 -> allergicTo (codedAllergies - 4) allergen
-    | i when i >= 2 && i < 4 -> allergicTo (codedAllergies - 2) allergen
-    | i when i >= 1 && i < 2 -> allergicTo (codedAllergies - 1) allergen
 
 let list codedAllergies =
-    failwith "You need to implement this function."
+    let rec helper (codedAllergies: int) (allergens: Allergen list) : Allergen list option =
+        match codedAllergies with
+        | i when i >= 256 -> helper (i - 256) allergens
+        | i when i >= 128 && i < 256 -> helper (codedAllergies - 128) (Cats :: allergens)
+        | i when i >= 64 && i < 128 -> helper (codedAllergies - 64) (Pollen :: allergens)
+        | i when i >= 32 && i < 64 -> helper (codedAllergies - 32) (Chocolate :: allergens)
+        | i when i >= 16 && i < 32 -> helper (codedAllergies - 16) (Tomatoes :: allergens)
+        | i when i >= 8 && i < 16 -> helper (codedAllergies - 8) (Strawberries :: allergens)
+        | i when i >= 4 && i < 8 -> helper (codedAllergies - 4) (Shellfish :: allergens)
+        | i when i >= 2 && i < 4 -> helper (codedAllergies - 2) (Peanuts :: allergens)
+        | i when i >= 1 && i < 2 -> helper (codedAllergies - 1) (Eggs :: allergens)
+        | i when i = 0 -> Some allergens
+        | _ -> None
 
 
+    match helper codedAllergies [] with
+    | Some allergens -> allergens
+    | None -> []
 
+let allergicTo codedAllergies allergen =
 
-allergicTo 1 Allergen.Eggs = true
+    match list codedAllergies with
+    | [] -> false
+    | elems -> elems |> List.contains allergen
