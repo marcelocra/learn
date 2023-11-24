2f8c5d7ac139a0b52e59d9f9b1340c11291c4eb6
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-11-24 15:47:38 -0300

    create project and basic example

---
+++ langs/elm/guide/src/Main.elm
@@ -0,0 +1,32 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+
+
+main =
+    Browser.sandbox { init = 0, update = update, view = view }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+view model =
+    div [ style "background-color" "red" ]
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
