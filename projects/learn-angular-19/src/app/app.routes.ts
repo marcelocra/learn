c76bcdb1a96e482246060bb34f46e27ada4a7ae3
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-11-22 19:26:05 -0300

    feat: add tw and create component

--- projects/learn-angular-19/src/app/app.routes.ts
+++ projects/learn-angular-19/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'counter',
+    component: CounterComponent,
+  },
+  {
+    path: '**',
+    component: AppComponent,
+  },
+];
